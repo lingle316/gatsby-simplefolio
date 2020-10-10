b4978603dffdba011518b1f9c41a3a76728dff27
Darryl <lingle316@yahoo.com>
2020-10-10 14:25:39 -0600

    Update portfolio data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,16 +2,16 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Darryl Lingle', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portfolio', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
-  title: '',
-  name: '',
-  subtitle: '',
+  title: 'Hello world',
+  name: 'Darryl Lingle',
+  subtitle: 'I am the Hero developer!',
   cta: '',
 };
 
